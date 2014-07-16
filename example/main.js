3299b8b7ccaff67b554045b73ab688133b8b32ee
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-16 15:01:21 -0600

    Adding support for switching tabs

--- example/main.js
+++ example/main.js
@@ -1,13 +1,18 @@
 /** @jsx React.DOM */
 var React = require('react'),
-	TabPanel = require('../lib/tabpanel');
+	TabPanel = require('../lib/main').TabPanel,
+	Tab = require('../lib/main').Tab;
 
 var App = React.createClass({
 	render: function () {
 		return (
 			<div>
 				<h1>React TabPanel</h1>
-				<TabPanel/>
+				<TabPanel>
+					<Tab label="Tab 1">Hello from Tab 1</Tab>
+					<Tab label="Tab 2">Hello from Tab 2</Tab>
+					<Tab label="Tab 3">Hello from Tab 3</Tab>
+				</TabPanel>
 			</div>
 		);
 	}
