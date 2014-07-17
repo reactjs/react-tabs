9a479709f6f278e51e864632c1a6b461f4084633
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-16 18:28:54 -0600

    Refactoring to be WAI-ARIA compliant

--- example/main.js
+++ example/main.js
@@ -1,18 +1,27 @@
 /** @jsx React.DOM */
 var React = require('react'),
-	TabPanel = require('../lib/main').TabPanel,
-	Tab = require('../lib/main').Tab;
+	ReactTabs = require('../lib/main'),
+	Tabs = ReactTabs.Tabs,
+	Tab = ReactTabs.Tab,
+	TabList = ReactTabs.TabList,
+	TabPanel = ReactTabs.TabPanel;
 
 var App = React.createClass({
 	render: function () {
 		return (
 			<div>
 				<h1>React TabPanel</h1>
-				<TabPanel>
-					<Tab label="Tab 1">Hello from Tab 1</Tab>
-					<Tab label="Tab 2">Hello from Tab 2</Tab>
-					<Tab label="Tab 3">Hello from Tab 3</Tab>
-				</TabPanel>
+				<Tabs>
+					<TabList>
+						<Tab>Tab 1</Tab>
+						<Tab>Tab 2</Tab>
+						<Tab>Tab 3</Tab>
+					</TabList>
+
+					<TabPanel>Hello from Tab 1</TabPanel>
+					<TabPanel>Hello from Tab 2</TabPanel>
+					<TabPanel>Hello from Tab 3</TabPanel>
+				</Tabs>
 			</div>
 		);
 	}
