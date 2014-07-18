f2fee2aa90888865ee2738f8ae22eac6b954ce46
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-18 13:09:04 -0600

    Testing is being held up by warnings

--- specs/helper.js
+++ specs/helper.js
@@ -1,2 +1,7 @@
-React = require('react/addons');
-TestUtils = React.addons.TestUtils;
+React		= require('react/addons');
+TestUtils	= React.addons.TestUtils;
+ReactTabs	= require('../lib/main');
+Tab			= ReactTabs.Tab;
+Tabs		= ReactTabs.Tabs;
+TabList		= ReactTabs.TabList;
+TabPanel	= ReactTabs.TabPanel;
--- specs/tabs.spec.js
+++ specs/tabs.spec.js
@@ -1,8 +1,23 @@
+/** @jsx React.DOM */
+
 require('./helper');
-var ReactTabs = require('../lib/main');
 
 describe('react-tabs', function () {
-	it('should do stuff', function () {
-		expect(true).toEqual(true);
+	it('create tabs', function () {
+		var tabs = (
+			<Tabs>
+				<TabList>
+					<Tab>Foo</Tab>
+				</TabList>
+				<TabPanel>
+					<h2>Foo</h2>
+				</TabPanel>
+			</Tabs>
+		);
+
+		// TODO: Need to fix some warnings for this to work
+		// See this fiddle for details - http://jsfiddle.net/mzabriskie/7v39Q/3/
+		// var component = TestUtils.renderIntoDocument(tabs);
+		// expect(document.querySelectorAll('.react-tabs').length).toEqual(1);
 	});
 });
