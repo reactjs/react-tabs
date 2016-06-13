f9a1dc6dfb27d653623d54bff8d95b5481c3f718
Daniel Tschinder <daniel@tschinder.de>
2016-06-13 22:36:56 +0200

    Add tests for main export

--- src/__tests__/main-test.js
+++ src/__tests__/main-test.js
@@ -1,15 +1,22 @@
 /* global describe, it, expect */
-import { Tab, Tabs, TabList, TabPanel } from '../main';
+import ReactTabs, { Tab, Tabs, TabList, TabPanel } from '../main';
 import TabComponent from '../components/Tab';
 import TabListComponent from '../components/TabList';
 import TabsComponent from '../components/Tabs';
 import TabPanelComponent from '../components/TabPanel';
 
-describe('<TabList />', () => {
-  it('should correctly export all components', () => {
+describe('main.js', () => {
+  it('should correctly export all component as named export', () => {
     expect(Tab).toEqual(TabComponent);
     expect(TabList).toEqual(TabListComponent);
     expect(Tabs).toEqual(TabsComponent);
     expect(TabPanel).toEqual(TabPanelComponent);
+  });
+
+  it('should correctly export all components as legacy es6 export', () => {
+    expect(ReactTabs.Tab).toEqual(TabComponent);
+    expect(ReactTabs.TabList).toEqual(TabListComponent);
+    expect(ReactTabs.Tabs).toEqual(TabsComponent);
+    expect(ReactTabs.TabPanel).toEqual(TabPanelComponent);
   });
 });
