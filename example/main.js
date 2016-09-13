fca1d1a4f244f75c52f0c45fd602b1f985dee9c8
Daniel Tschinder <daniel@tschinder.de>
2016-09-13 22:08:22 +0200

    Fix link

--- example/main.js
+++ example/main.js
@@ -12,7 +12,7 @@ var App = React.createClass({
 			<div>
 				<h1>React Tabs</h1>
 				<p>
-					<a href="https://github.com/rackt/react-tabs/edit/gh-pages/example/main.js">Demo Source</a>
+					<a href="https://github.com/reactjs/react-tabs/blob/gh-pages/example/main.js">Demo Source</a>
 				</p>
 				<p>
 					<em>Hint:</em>
