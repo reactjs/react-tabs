3bf3d688ecd15e7894b6858a0e9b63ed11bcfff6
Daniel Tschinder <code@tschinder.de>
2016-09-15 11:11:56 +0200

    example: Not mutate state and correctly handle selectedIndex [skip ci]

--- examples/dyno/app.js
+++ examples/dyno/app.js
@@ -4,7 +4,6 @@ import Modal from 'react-modal';
 import { Tab, Tabs, TabList, TabPanel } from '../../src/main';
 
 Modal.setAppElement(document.getElementById('example'));
-Modal.injectCSS();
 
 const App = React.createClass({
   getInitialState() {
@@ -18,43 +17,6 @@ const App = React.createClass({
         { label: 'Zap', content: 'This is zap' },
       ],
     };
-  },
-
-  render() {
-    return (
-      <div style={{ padding: 50 }}>
-        <p>
-          <button onClick={this.openModal}>+ Add</button>
-        </p>
-        <Tabs selectedIndex={this.state.selectedIndex}>
-          <TabList>
-            {this.state.tabs.map((tab, i) => {
-              return (
-                <Tab key={i}>
-                  {tab.label} <a href="#" onClick={this.removeTab.bind(this, i)}>✕</a>
-                </Tab>
-              );
-            })}
-          </TabList>
-          {this.state.tabs.map((tab, i) => {
-            return <TabPanel key={i}>{tab.content}</TabPanel>;
-          })}
-        </Tabs>
-        <Modal
-          isOpen={this.state.isModalOpen}
-          onRequestClose={this.closeModal}
-          style={{ width: 400, height: 350, margin: '0 auto' }}
-        >
-          <h2>Add a Tab</h2>
-          <label htmlFor="label">Label:</label><br />
-          <input id="label" type="text" ref="label" /><br /><br />
-          <label htmlFor="content">Content:</label><br />
-          <textarea id="content" ref="content" rows="10" cols="50"></textarea><br /><br />
-          <button onClick={this.addTab}>OK</button>{' '}
-          <button onClick={this.closeModal}>Cancel</button>
-        </Modal>
-      </div>
-    );
   },
 
   openModal() {
@@ -73,19 +35,57 @@ const App = React.createClass({
     const label = this.refs.label.value;
     const content = this.refs.content.value;
 
-    this.state.tabs.push({
-      label: label,
-      content: content,
-    });
     this.setState({
-      selectedIndex: this.state.tabs.length - 1,
+      tabs: [
+        ...this.state.tabs,
+        { label, content },
+      ],
+      selectedIndex: this.state.tabs.length,
     });
     this.closeModal();
   },
 
   removeTab(index) {
-    this.state.tabs.splice(index, 1);
-    this.forceUpdate();
+    this.setState({
+      tabs: this.state.tabs.filter((tab, i) => i !== index),
+      selectedIndex: this.state.selectedIndex - 1,
+    });
+  },
+
+  render() {
+    return (
+      <div style={{ padding: 50 }}>
+        <p>
+          <button onClick={this.openModal}>+ Add</button>
+        </p>
+        <Tabs
+          selectedIndex={this.state.selectedIndex}
+          onSelect={selectedIndex => this.setState({ selectedIndex })}
+        >
+          <TabList>
+            {this.state.tabs.map((tab, i) => (
+              <Tab key={i}>
+                {tab.label} <a href="#" onClick={() => this.removeTab(i)}>✕</a>
+              </Tab>
+            ))}
+          </TabList>
+          {this.state.tabs.map((tab, i) => <TabPanel key={i}>{tab.content}</TabPanel>)}
+        </Tabs>
+        <Modal
+          isOpen={this.state.isModalOpen}
+          onRequestClose={this.closeModal}
+          style={{ width: 400, height: 350, margin: '0 auto' }}
+        >
+          <h2>Add a Tab</h2>
+          <label htmlFor="label">Label:</label><br />
+          <input id="label" type="text" ref="label" /><br /><br />
+          <label htmlFor="content">Content:</label><br />
+          <textarea id="content" ref="content" rows="10" cols="50" /><br /><br />
+          <button onClick={this.addTab}>OK</button>{' '}
+          <button onClick={this.closeModal}>Cancel</button>
+        </Modal>
+      </div>
+    );
   },
 });
 
