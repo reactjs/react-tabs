3e5127be70aa98c3e8d16dba655f5b0982196029
exabugs <exabugs@gmail.com>
2017-09-04 07:14:07 +0900

    Always callback setSelected. (#195)
    * Always callback setSelected.
    
    * Always callback setSelected. (npm run bundle)
    
    * Add test

--- dist/react-tabs.js
+++ dist/react-tabs.js
@@ -870,8 +870,6 @@ var UncontrolledTabs = function (_Component) {
   }
 
   UncontrolledTabs.prototype.setSelected = function setSelected(index, event) {
-    // Don't do anything if nothing has changed
-    if (index === this.props.selectedIndex) return;
     // Check index boundary
     if (index < 0 || index >= this.getTabsCount()) return;
 
