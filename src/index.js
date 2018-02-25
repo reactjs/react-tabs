5c69fa2d30cf611cd320a85faa38d1c2986b029e
David Narbutovich <vamnemne@gmail.com>
2018-02-25 12:30:54 +0300

    Use auto export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import Tabs from './components/Tabs';
-import TabList from './components/TabList';
-import Tab from './components/Tab';
-import TabPanel from './components/TabPanel';
-import { reset as resetIdCounter } from './helpers/uuid';
-
-export { Tab, TabList, TabPanel, Tabs, resetIdCounter };
+export { default as Tabs } from "./components/Tabs";
+export { default as TabList } from "./components/TabList";
+export { default as Tab } from "./components/Tab";
+export { default as TabPanel } from "./components/TabPanel";
+export { reset as resetIdCounter } from "./helpers/uuid";
