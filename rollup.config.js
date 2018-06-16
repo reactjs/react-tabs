e9559ef8bdc7043631c1f0f94a4f65c9059fd565
Daniel Tschinder <daniel.tschinder@researchgate.net>
2018-06-16 20:32:39 +0200

    chore: Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import ignore from 'rollup-plugin-ignore';
 
 const devConfig = {
