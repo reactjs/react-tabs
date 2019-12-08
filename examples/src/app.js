d5cc23cd93f68abfbdc652b757d16639ad393c0f
Morta1 <mortaob@gmail.com>
2019-12-08 21:11:12 +0200

    Added right to left support.

--- examples/src/app.js
+++ examples/src/app.js
@@ -5,12 +5,14 @@ import './example.less';
 import SuperMario from './components/examples/SuperMario';
 import MattGroening from './components/examples/MattGroening';
 import Avengers from './components/examples/Avengers';
+import RightToLeft from './components/examples/RightToLeft';
 
 ReactDOM.render(
   <div>
     <SuperMario />
     <MattGroening />
     <Avengers />
+    <RightToLeft />
   </div>,
   document.getElementById('example'),
 );
